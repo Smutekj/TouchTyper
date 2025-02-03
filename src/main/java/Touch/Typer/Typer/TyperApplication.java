b31e9a7263e50a05262d69d1890e10a7eec17fae
DrHavran <krysacy@gmail.com>
2025-02-03 15:38:18 +0100

    simple changes

--- src/main/java/Touch/Typer/Typer/TyperApplication.java
+++ src/main/java/Touch/Typer/Typer/TyperApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TyperApplication {
+public class  TyperApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TyperApplication.class, args);
